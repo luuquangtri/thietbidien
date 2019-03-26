b5d271e1d2cd531e4e1890c7d3ff30a45ec46a23
luuquangtri <48348502+luuquangtri@users.noreply.github.com>
2019-03-26 17:27:40 +0700

    dsd

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
         if ($(window).scrollTop() > 135) {
             $('.menu-slide-bar ').css('position', 'fixed');
             $('.menu-slide-bar ').css('top', '40px');
-            $('.menu-slide-bar ').css('width', '225');
+            $('.menu-slide-bar ').css('width', '285');
         }
         else if ($(window).scrollTop() <= 135) {
             $('.menu-slide-bar').css('position', '');
             $('.menu-slide-bar').css('top', '');
-            $('.menu-slide-bar ').css('width', '225');
+            $('.menu-slide-bar ').css('width', '285');
         }
         if ($('.menu-slide-bar').offset().top + $(".menu-slide-bar").height() > $(".footer").offset().top) {
             $('.menu-slide-bar').css('top', -($(".menu-slide-bar").offset().top + $(".menu-slide-bar").height() - $(".footer").offset().top));
