4ca2ca5497b776b424e641da05beb4059064d4c6
luuquangtri <48348502+luuquangtri@users.noreply.github.com>
2019-03-31 00:14:30 +0700

    ds

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             600: {
                 items: 1    
             },
-            1000: {
+            1024: {
                 items: 1
             }
         }
@@ -23,9 +23,18 @@ $(document).ready(function () {
         slidesToScroll: 4,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1200,
                 settings: {
                     slidesToShow: 4,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
                     slidesToScroll: 3,
                     infinite: true,
                     dots: true
--- js/scroll-top.js
+++ js/scroll-top.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
         if ($(window).scrollTop() > 135) {
             $('.menu-slide-bar ').css('position', 'fixed');
             $('.menu-slide-bar ').css('top', '40px');
-            $('.menu-slide-bar ').css('width', '285');
+            $('.menu-slide-bar ').css('width', '245');
         }
         else if ($(window).scrollTop() <= 135) {
             $('.menu-slide-bar').css('position', '');
             $('.menu-slide-bar').css('top', '');
-            $('.menu-slide-bar ').css('width', '285');
+            $('.menu-slide-bar ').css('width', '245');
         }
         if ($('.menu-slide-bar').offset().top + $(".menu-slide-bar").height() > $(".footer").offset().top) {
             $('.menu-slide-bar').css('top', -($(".menu-slide-bar").offset().top + $(".menu-slide-bar").height() - $(".footer").offset().top));
