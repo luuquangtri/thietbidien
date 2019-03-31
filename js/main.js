3da414b12f8e1e9fd8b803e85b7add3a35cfc2b6
luuquangtri <48348502+luuquangtri@users.noreply.github.com>
2019-03-31 14:57:22 +0700

    dsds

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 4,
                     slidesToScroll: 3,
                     infinite: true,
                     dots: true
